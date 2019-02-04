748b13668bad80927983b8e18d53865a6dd46090
Тажибек Акнур Арманбеккызы <@corp.itstep>
2019-02-04 22:03:27 +0600

    first commit

---
+++ WeatherWPF/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+ 
+using Newtonsoft.Json;
+using WeatherWPF;
+
+namespace WeatherWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private WeatherResponse GetJson(string city)
+        {
+            string url = $"http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid=67a3dc729fe825c7287ebe1fe4a57585";
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+
+            HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+            string response;
+
+            using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+            {
+                response = streamReader.ReadToEnd();
+            }
+
+            WeatherResponse weatherResponse = JsonConvert.DeserializeObject<WeatherResponse>(response);
+            return weatherResponse;
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            WeatherResponse weatherResponse = GetJson(city.Text);
+            temp.Text = weatherResponse.Main.Temp.ToString();
+        }
+    }
+}
